5ea7beca01586e1f0f5b6ee0d6f69f4dfaaeffb3
Madhawa-Tharindu <madhawath@gmail.com>
2024-04-02 23:40:46 +0530

    created update and post delete method

--- routes/posts.js
+++ routes/posts.js
@@ -36,4 +36,71 @@ router.get('/posts', (req, res) => {
         });
 });
 
+// update an user
+router.put('/post/update/:id', (req, res) => {
+    Post.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        .then(updatedPost => {
+            if (!updatedPost) {
+                return res.status(404).json({
+                    error: "Post not found"
+                });
+            }
+            return res.status(200).json({
+                success: "Post updated successfully!",
+                updatedPost
+            });
+        })
+        .catch(err => {
+            return res.status(400).json({
+                error: err.message
+            });
+        });
+});
+
+// // and also we can write above update code below as well
+// // Update a post
+// router.put('/post/update/:id', async (req, res) => {
+//     try {
+//         const updatedPost = await Post.findByIdAndUpdate(req.params.id, req.body, { new: true });
+
+//         if (!updatedPost) {
+//             return res.status(404).json({
+//                 error: "Post not found"
+//             });
+//         }
+
+//         return res.status(200).json({
+//             success: "Post updated successfully!",
+//             updatedPost
+//         });
+//     } catch (err) {
+//         return res.status(400).json({
+//             error: err.message
+//         });
+//     }
+// });
+
+
+// delete a post
+router.delete('/post/delete/:id', (req, res) => {
+    Post.findByIdAndDelete(req.params.id)
+        .then(deletedPost => {
+            if (!deletedPost) {
+                return res.status(404).json({
+                    error: "Post not found"
+                });
+            }
+            return res.status(200).json({
+                success: "Post deleted successfully!",
+                deletedPost
+            });
+        })
+        .catch(err => {
+            return res.status(400).json({
+                error: err.message
+            });
+        });
+})
+
+
 export default router;
