30d0358741b7b6f5470b8caee23713f364582dc9
Madhawa-Tharindu <madhawath@gmail.com>
2024-04-02 18:53:07 +0530

    DB Connected

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import connectDB from './config/connectDB.js';
+import cors from 'cors';
+
+const app = express();
+
+//CORS Policy
+app.use(cors())
+
+const port = process.env.PORT
+const DATABASE_URL = process.env.DATABASE_URL
+
+//Database connection
+connectDB(DATABASE_URL);
+
+app.listen(port, () => {
+    console.log(`App is running on port ${port}`)
+});
 
