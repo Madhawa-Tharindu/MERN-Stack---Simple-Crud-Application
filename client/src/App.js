059c7d24a7acafce219d067b856eb5c86c04b2fd
Madhawa-Tharindu <madhawath@gmail.com>
2024-04-03 14:58:05 +0530

    write the code using class component to get an idea

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// get an idea about the class component vs functional component
+// initially I have created a class component for getting all post
+
+import React, { Component } from 'react';
+import axios from 'axios';
+// axios is a promise based HTTP client for the browser and node.js
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      posts: [],
+    }
+  }
+  //componentDidMount() is invoked immediately after a component is mounted. 
+  componentDidMount() {
+    this.getPosts();
+  }
+
+
+  getPosts() {
+    axios.get('http://localhost:8000/posts')
+          .then(res => {
+            if (res.data.success) {
+              this.setState({
+                posts: res.data.existingPosts
+              })
+              console.log(res.data, "data was received successfully!");
+            }
+          })
+          .catch(err => {
+            alert(err + "Error while fetching data");
+          });
+  }
+
+  render() {
+    return (
+        <div>
+            <h1>Posts</h1>
+            {this.state.posts.map(post => (
+              <div key={post.id}>
+              <h2>{post.topic}</h2>
+              <h2>{post.description}</h2>
+              <h2>{post.postCategory}</h2>
+            </div>
+            ))}
+        </div>
+    )
 }
-
-export default App;
+} 
