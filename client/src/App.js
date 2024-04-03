22f25b0f35f4345219559d3ac20b3e8b765d2072
Madhawa-Tharindu <madhawath@gmail.com>
2024-04-03 15:14:48 +0530

    create fuctional component code for getPosts

--- client/src/App.js
+++ client/src/App.js
@@ -54,3 +54,44 @@ export default class App extends Component {
 
 
 */
+
+
+// functional component using ES6 syntax
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const App = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  const getPosts = () => {
+    axios.get('http://localhost:8000/posts')
+       .then(res => {
+        if (res.data.success === true) {
+          setPosts(res.data.existingPosts);
+          console.log(res.data, "data was received successfully!");
+        }
+       })
+       .catch(err => {
+        alert(err + "Error while fetching posts!");
+       })
+  }
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      {posts.map(post => (
+        <div key={post.id}>
+          <h2>{post.topic}</h2>
+          <h2>{post.description}</h2>
+          <h2>{post.postCategory}</h2>
+        </div>))}
+    </div>
+  )
+
+}
+
+export default App;
