ecd8b05016ad16acee38a2d10f339ebf6514e262
Madhawa-Tharindu <madhawath@gmail.com>
2024-04-04 06:00:15 +0530

    fix the error in App.js filr path

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,20 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import CreatePost from './components/CreatePost';
 import EditPost from './components/EditPost';
-import PostDetails from  './components/PostDetails';
+import PostDetail from  './components/PostDetail';
+import NavBar from './components/NavBar';
 
 const App = () => {
   return (
     <Router>
       <Routes>
+        <div className='container'>
+          <NavBar />
         <Route exact path="/" element={<Home />} />
         <Route exact path="/add" element={<CreatePost />} />
         <Route exact path="/edit" element={<EditPost />} />
-        <Route exact path="/post/:id" element={<PostDetails />} />
+        <Route exact path="/post/:id" element={<PostDetail />} />
+        </div>
         </Routes>
     </Router>
   )
